01088bccdcb8c972e5dd594e4cdb57b85a1737f3
Yannis Febvre <yannisfebvre@gmail.com>
2025-10-06 16:40:43 +0200

    analyticis

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NoiseVideo from "./components/NoiseVideo";
+import { Analytics } from "@vercel/analytics/next"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,6 +56,7 @@ export default function RootLayout({
       >
         {children}
         <NoiseVideo />
+        <Analytics />
       </body>
     </html>
   );
