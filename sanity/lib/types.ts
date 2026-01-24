45bb5bbee90b7679cd58c2f5ee61a94a198b0a96
Yannis <yannisfebvre@gmail.com>
2026-01-24 15:32:25 +0100

    d

--- sanity/lib/types.ts
+++ sanity/lib/types.ts
@@ -1,4 +1,4 @@
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import type { SanityImageSource } from "@sanity/image-url";
 
 // Type pour les assets d'image Sanity
 export interface SanityImageAsset {
